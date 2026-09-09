a79c2cd38c46a81a00021d064c9ce37a14fa22af
anthonymanuelm97 <anthonymanuelm97@gmail.com>
2026-09-08 21:19:25 -0400

    feat: added search product function and search product option

--- main.py
+++ main.py
@@ -5,11 +5,19 @@ business_catalog = [
 ]
 
 
+def search_product(catalog, name_searched):
+    for product in catalog:
+        if product["name"] == name_searched:
+            return product
+    return None
+
+
 def main():
     while True:
         print("Menu:")
         print("0. Exit")
         print("1. View catalog")
+        print("2. Search a product")
 
         option = input("Choose an option: ")
 
@@ -20,6 +28,15 @@ def main():
         if option == "1":
             for product in business_catalog:
                 print(f"{product['name']}: ${product['price']}")
+
+        if option == "2":
+            name_searched = input("Enter the product name: ")
+            product = search_product(business_catalog, name_searched)
+
+            if product is not None:
+                print(product)
+            else:
+                print("product not found")
 
 
 if __name__ == "__main__":
